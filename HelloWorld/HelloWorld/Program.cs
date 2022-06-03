632a4009830ff8ee25d4400e5833bb5dd526c887
Cristian Leiton Valencia <cristianleitonvalencia@gmail.com>
2022-06-02 20:11:24 -0500

    ADD - Ejemplo Strings and Numerics

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's your name? Please write your full name");
+            string fullUserName = Console.ReadLine();
+            Console.WriteLine("Hello " + fullUserName + ", Welcome to Platzi");
+        }
+    }
+}
+
---
+++ NumericCalculator/NumericCalculator/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace NumericCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int numberOne;
+            int numberTwo;
+
+            Console.WriteLine("Input the first number:");
+            numberOne = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Input the second number");
+            numberTwo = Convert.ToInt32(Console.ReadLine());
+
+            int result = numberOne * numberTwo;
+
+            Console.WriteLine("The result of " + numberOne + " * " + numberTwo + " is: " + result);
+
+
+        }
+    }
+}
+
