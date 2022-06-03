083e772288f394f45feb7f5736e7fcee2f351091
Cristian Leiton Valencia <cristianleitonvalencia@gmail.com>
2022-06-02 20:28:52 -0500

    ADD - Ejemplo string interpolation

---
+++ ManejoStrings/ManejoStrings/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ManejoStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int height = 170;
+            int age = 25;
+            string name = "Cristian Leiton";
+            string information = "The requested info is:\n\nuser name: " + name
+                + "\nuser age: " + age + "\nuser height: " + height + "\n\n\n";
+
+            string informationInterpolation = $"(interpolation)The requested info is:\n\nuser name: {name}\nuser age: {age}\nuser height: {height}";
+
+            Console.WriteLine(information);
+            Console.WriteLine(informationInterpolation);
+        }
+    }
+}
+
